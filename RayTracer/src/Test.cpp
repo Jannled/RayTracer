c321b13a3f2eeb996988f4a4a9b240ad11aab123
Jannled <jannled@live.de>
2018-01-25 23:15:47 +0100

    Starting to write own RayTracer

--- RayTracer/src/Test.cpp
+++ RayTracer/src/Test.cpp
@@ -1,75 +1,76 @@
 #include <stdlib.h>   // card > aek.ppm
 #include <stdio.h>
 #include <math.h>
-typedef int i;
-typedef float f;
+typedef int int;
 
-	struct v
+	struct vertice
 	{
-		f x,y,z;
-		v operator+(v r)
+		float x,y,z;
+		vertice operator+(vertice r)
 		{
-			return v(x+r.x,y+r.y,z+r.z);
+			return vertice(x + r.x, y + r.y, z+r.z);
 		}
 		
-		v operator*(f r)
+		vertice operator*(float r)
 		{
-			return v(x*r,y*r,z*r);
+			return vertice(x * r, y * r, z * r);
 		}
 		
-		f operator%(v r)
+		float operator%(vertice r)
 		{
-			return x*r.x+y*r.y+z*r.z;
+			return x * r.x + y * r.y + z * r.z;
 		}
 		
-		v(){}v 
+		vertice(){}vertice
 		
-		operator^(v r)
+		operator^(vertice r)
 		{
-			return v(y*r.z-z*r.y,z*r.x-x*r.z,x*r.y-y*r.x);
+			return vertice(y * r.z - z * r.y, z * r.x - x * r.z, x * r.y - y * r.x);
 		}
 		
-		v(f a,f b,f c)
+		vertice(float a, float b, float c)
 		{
-			x=a;y=b;z=c;
+			x = a;
+			y = b;
+			z = c;
 		}
 		
-		v operator!()
+		vertice operator!()
 		{
 			return*this*(1/sqrt(*this%*this));
 		}
 	};
 	
-	i G[]={247570,280596,280600,249748,18578,18577,231184,16,16};
+	int G[] = {247570, 280596, 280600, 249748, 18578, 18577, 231184, 16, 16};
 	
-	f R()
+	float R()
 	{
-		return(f)rand()/RAND_MAX;
+		return (float) rand() / RAND_MAX;
 	}
 
-	i T(v o,v d,f&t,v&n)
+	int T(vertice o, vertice d, float& t, vertice& n)
 	{
-		t=1e9;
-		i m=0;
-		f p=-o.z/d.z;
+		t = 1e9;
+		int m = 0;
+		float p = -o.z / d.z;
 		
-		if(.01<p)
-			t=p,n=v(0,0,1),m=1;
+		if(0.01 < p)
+			t = p, n = vertice(0, 0, 1), m = 1;
 			
-		for(i k=19;k--;)
+		for(int k=19;k--;)
 		{
-			for(i j=9;j--;)
+			for(int j=9; j--;)
 			{
-				if(G[j]&1<<k)
+				if(G[j] & 1<<k)
 				{
-					v p=o+v(-k,0,-j-4);
-					f b=p%d,c=p%p-1,q=b*b-c;
+					vertice p = o + vertice(-k, 0, -j-4);
+					float b = p%d, c = p%p-1, q = b * b - c;
 
-					if(q>0)
+					if(q > 0)
 					{
-						f s=-b-sqrt(q);
-						if(s<t&&s>.01)
-							t=s,n=!(p+d*t),m=2;
+						float s=-b-sqrt(q);
+						if(s < t&&s > 0.01)
+							t = s, n = !(p+d*t), m = 2;
 					}
 				}
 			}
@@ -77,46 +78,46 @@ typedef float f;
 		return m;
 	}
 
-	v S(v o,v d)
+	vertice S(vertice o, vertice d)
 	{
-		f t;
-		v n;
-		i m=T(o,d,t,n);
+		float t;
+		vertice n;
+		int m = T(o, d, t, n);
 
 		if(!m)
-			return v(.7,.6,1)*pow(1-d.z,4);
+			return vertice(.7,.6,1)*pow(1-d.z,4);
 
-		v h=o+d*t,l=!(v(9+R(),9+R(),16)+h*-1),r=d+n*(n%d*-2);
-		f b=l%n;
+		vertice h=o+d*t,l=!(vertice(9+R(),9+R(),16)+h*-1),r=d+n*(n%d*-2);
+		float b=l%n;
 
 		if(b<0||T(h,l,t,n))
 			b=0;
 
-		f p=pow(l%r*(b>0),99);
+		float p=pow(l%r*(b>0),99);
 
 		if(m&1)
 		{
 			h=h*.2;
-			return ((i)(ceil(h.x)+ceil(h.y))&1?v(3,1,1):v(3,3,3))*(b*.2+.1);
+			return ((int)(ceil(h.x)+ceil(h.y))&1?vertice(3,1,1):vertice(3,3,3))*(b*.2+.1);
 		}
-		return v(p,p,p)+S(h,r)*.5;}
+		return vertice(p,p,p)+S(h,r)*.5;}
 
-	i main()
+	int main()
 	{
 		printf("P6 512 512 255 ");
-		v g=!v(-6,-16,0),a=!(v(0,0,1)^g)*.002,b=!(g^a)*.002,c=(a+b)*-256+g;
+		vertice g=!vertice(-6,-16,0),a=!(vertice(0,0,1)^g)*.002,b=!(g^a)*.002,c=(a+b)*-256+g;
 
-		for(i y=512;y--;)
+		for(int y=512;y--;)
 		{
-			for(i x=512;x--;)
+			for(int x=512;x--;)
 			{
-				v p(13,13,13);
-				for(i r=64;r--;)
+				vertice p(13,13,13);
+				for(int r=64;r--;)
 				{
-					v t=a*(R()-.5)*99+b*(R()-.5)*99;
-					p=S(v(17,16,8)+t,!(t*-1+(a*(R()+x)+b*(y+R())+c)*16))*3.5+p;
+					vertice t=a*(R()-.5)*99+b*(R()-.5)*99;
+					p=S(vertice(17,16,8)+t,!(t*-1+(a*(R()+x)+b*(y+R())+c)*16))*3.5+p;
 				}
-				printf("%c%c%c",(i)p.x,(i)p.y,(i)p.z);
+				printf("%c%c%c",(int)p.x,(int)p.y,(int)p.z);
 			}
 		}
 	}
---
+++ RayTracer/src/com/github/jannled/raytracer/Main.java
@@ -0,0 +1,6 @@
+package com.github.jannled.raytracer;
+
+public class Main
+{
+
+}
